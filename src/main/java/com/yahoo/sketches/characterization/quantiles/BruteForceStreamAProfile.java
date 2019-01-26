dfa4bfc2955bf0572509d6040b73348f8e65d76e
lrhodes <lrhodes@yahoo-inc.com>
2019-01-26 00:17:50 -0800

    updated gitignore, added brute-force

---
+++ src/main/java/com/yahoo/sketches/characterization/quantiles/BruteForceStreamAProfile.java
@@ -0,0 +1,302 @@
+/*
+ * Copyright 2019, Yahoo! Inc. Licensed under the terms of the
+ * Apache License 2.0. See LICENSE file at the project root for terms.
+ */
+
+package com.yahoo.sketches.characterization.quantiles;
+
+import static com.yahoo.sketches.Util.ceilingPowerOf2double;
+import static com.yahoo.sketches.Util.log2;
+import static com.yahoo.sketches.Util.pwr2LawNextDouble;
+import static java.lang.Math.rint;
+import static java.lang.Math.round;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.testng.annotations.Test;
+
+import com.yahoo.sketches.LineReader;
+import com.yahoo.sketches.ProcessLine;
+import com.yahoo.sketches.UnzipFiles;
+import com.yahoo.sketches.characterization.Job;
+import com.yahoo.sketches.characterization.JobProfile;
+import com.yahoo.sketches.characterization.Properties;
+
+/**
+ * @author Lee Rhodes
+ */
+public class BruteForceStreamAProfile implements JobProfile {
+
+  private Job job;
+
+  //Properties
+  private String srcFileName;
+  private int reportInterval; //prints number of lines read to console every reportInterval lines.
+  private int numRanks; //number of linearly spaced ranks between zero and one.
+  private int ppo; //number of split-points per Octave.
+  private String cdfHdr;
+  private String cdfFmt;
+  private String pmfHdr;
+  private String pmfFmt;
+
+  //processing input array
+  private boolean dataWasZipped = false;
+  private static final int numItems = 263078000;
+  private int[] dataArr = new int[numItems];
+
+  //outputs for plotting
+  private double minV;
+  private double maxV;
+  private double[] ranksArr; // 0, .01, .02, ...1.0 //101 ranks
+  private double[] quantilesArr; //101 values
+
+  private double[] spArr;  //splitpoints 1 ... ceilPwr2(maxValue), ppo values per octave
+  private int numSP;
+  private long[] spCounts;  //
+  private Process proc = new Process();
+
+
+  //JobProfile
+  @Override
+  public void start(final Job job) {
+    this.job = job;
+    final Properties prop = job.getProperties();
+
+    //Get Properties
+    srcFileName = prop.mustGet("FileName");
+    reportInterval = Integer.parseInt(prop.mustGet("ReportInterval"));
+    numRanks = Integer.parseInt(prop.mustGet("NumRanks"));
+    ppo = Integer.parseInt(prop.mustGet("PPO"));
+
+    cdfHdr = prop.mustGet("CdfHdr").replace("\\t", "\t");
+    cdfFmt = prop.mustGet("CdfFmt").replace("\\t", "\t");
+    pmfHdr = prop.mustGet("PdfHdr").replace("\\t", "\t");
+    pmfFmt = prop.mustGet("PdfFmt").replace("\\t", "\t");
+
+    ranksArr = buildRanksArr(numRanks);
+    quantilesArr = new double[numRanks];
+    checkMonotonic(ranksArr);
+
+    processInputStream();
+
+    if (dataWasZipped) { //after we are done
+      final File file = new File(srcFileName);
+      if (file.exists()) { file.delete(); }
+    }
+  }
+
+  /**
+   * Read file, Print CDF, Print PMF.
+   */
+  private void processInputStream() {
+    checkIfZipped(srcFileName);
+    long startTime_nS;
+
+    //Read
+    println("Input Lines Processed: ");
+    final LineReader lineReader = new LineReader(srcFileName);
+    startTime_nS = System.nanoTime();
+    lineReader.read(0, proc);
+    final long readTime_nS = System.nanoTime() - startTime_nS;
+    assert proc.n == numItems;
+
+    //Sort
+    println("Sort input data");
+    startTime_nS = System.nanoTime();
+    Arrays.sort(dataArr);
+    final long sortTime_nS = System.nanoTime() - startTime_nS;
+    println("Sort done");
+
+    //Compute CDF & PMF
+    minV = dataArr[0];
+    maxV = dataArr[numItems - 1];
+    assert maxV > minV;
+
+    //outputs
+    //ranksArr and quantilesArr already initialized
+    spArr = buildSplitPointsArr(minV, maxV, ppo);
+    checkMonotonic(spArr);
+    numSP = spArr.length;
+    spCounts = new long[numSP];
+
+    println("Process Array");
+    startTime_nS = System.nanoTime();
+    processArray(dataArr, spArr, spCounts, ranksArr, quantilesArr);
+    final long processTime_nS = System.nanoTime() - startTime_nS;
+    println("End Process Array");
+
+    printCDF();
+    printPMF();
+    printTimes(readTime_nS, sortTime_nS, processTime_nS);
+  }
+
+  //initialized with values: ranksArr, spArr
+  private static void processArray(final int[] sortedArr, final double[] spArr, final long[] spCounts,
+      final double[] ranksArr, final double[] quantilesArr) {
+    int rankIdx = 0;
+    int spIdx = 0;
+    final int dataLen = sortedArr.length;
+    final int dataLenM1 = dataLen - 1;
+    final int spLen = spArr.length;
+    double lastV = sortedArr[0];
+    for (int i = 0; i < dataLen; i++) {
+      final double v = sortedArr[i];
+      if (v < spArr[spIdx]) {
+        spCounts[spIdx]++;
+      } else {
+        while (((spIdx + 1) < spLen) && (v >= spArr[spIdx])) {
+          spIdx++;
+        }
+        spCounts[spIdx]++;
+      }
+      if (i == dataLenM1) {
+        quantilesArr[quantilesArr.length - 1] = sortedArr[dataLenM1];
+      }
+      if (i >= (ranksArr[rankIdx] * dataLen)) {
+        quantilesArr[rankIdx] = lastV;
+        rankIdx++;
+      }
+      lastV = v;
+    }
+  }
+
+  @Test
+  public void checkProcessArr() {
+    final int[] data = new int[10000];
+    for (int i = 0; i < data.length; i++) { data[i] = i; }
+    final double[] spArr = buildSplitPointsArr(0, 9999, 1);
+    final long[] spCounts = new long[spArr.length];
+    final double[] ranksArr = buildRanksArr(101);
+    final double[] quantilesArr = new double[101];
+    processArray(data, spArr, spCounts, ranksArr, quantilesArr);
+    System.out.println("Done");
+  }
+
+  /**
+   * Compute the split-points for the PMF function.
+   * The minimum split-point will never be less than one.
+   * This assumes all points are positive and may include zero.
+   * @param min The minimum value recorded by the sketch
+   * @param max The maximum value recorded by the sketch
+   * @param ppo desired number of points per Octave.
+   * @return the split-points array, which does not include zero
+   */
+  private double[] buildSplitPointsArr(final double min, final double max, final int ppo) {
+    final double ceilPwr2min = ceilingPowerOf2double(min);
+    final double ceilPwr2max = ceilingPowerOf2double(max);
+    final int numSP = (int)((round(log2(ceilPwr2max)) - round(log2(ceilPwr2min)) ) * ppo) + 1;
+    spArr = new double[numSP];
+    spArr[0] = ceilPwr2min;
+    double next = ceilPwr2min;
+    for (int i = 1; i < numSP; i++) {
+      next = pwr2LawNextDouble(ppo, next, false);
+      spArr[i] = next;
+    }
+    return spArr;
+  }
+
+  private static void checkMonotonic(final double[] arr) {
+    final int len = arr.length;
+    for (int i = 1; i < len; i++) {
+      assert arr[i] > arr[i - 1];
+    }
+  }
+
+  @Test
+  public void checkBuildSPArr() {
+    final double[] arr = buildSplitPointsArr(512, 999, 2);
+    for (int i = 0; i < arr.length; i++) { System.out.println("" + arr[i]); }
+  }
+
+  /**
+   * Compute the ranks array.
+   * @param numRanks the number of evenly-spaced rank values including 0 and 1.0.
+   * @return the ranks array
+   */
+  private static double[] buildRanksArr(final int numRanks) {
+    final int numRanksM1 = numRanks - 1;
+    final double[] fractions = new double[numRanks];
+    final double delta = 1.0 / (numRanksM1);
+    double d = 0.0;
+    for (int i = 0; i < numRanks; i++) {
+      fractions[i] = d;
+      d += delta;
+      d = rint(d * numRanksM1) / numRanksM1;
+    }
+    return fractions;
+  }
+
+  private void printCDF() {
+    println("CDF");
+    println(String.format(cdfHdr, "Index", "Rank", "Quantile"));
+    for (int i = 0; i < numRanks; i++) {
+      final String s = String.format(cdfFmt, i, ranksArr[i], quantilesArr[i]);
+      println(s);
+    }
+    println("");
+  }
+
+  private void printPMF() {
+    println("PMF");
+    println(String.format(pmfHdr, "Index", "Quantile", "Mass"));
+    int i;
+    for (i = 0; i < numSP; i++) {
+      println(String.format(pmfFmt, i, spArr[i], spCounts[i]));
+    }
+  }
+
+  private void printTimes(final long readTime_nS, final long sortTime_nS, final long processTime_nS) {
+    println("");
+    final double readTime_S = readTime_nS / 1E9;
+    println(String.format("ReadTime_Sec  :\t%10.3f", readTime_S));
+    println(String.format("ReadRate/Sec  :\t%,10.0f", proc.n / readTime_S));
+    println(String.format("SortTime_nS   :\t%,10d", sortTime_nS));
+    println(String.format("ProcessTime_mSec  :\t%10.3f", processTime_nS / 1E6));
+    println(String.format("PT/Point_nSec:\t%10.3f", (double)processTime_nS / numRanks));
+  }
+
+  private void checkIfZipped(final String srcFileName) {
+    final File file = new File(srcFileName);
+    if (!file.exists()) {
+      final String srcZipFile = srcFileName + ".zip";
+      final File zipFile = new File(srcZipFile);
+      if (!zipFile.exists()) {
+        throw new IllegalArgumentException("Neither file nor zipFile exists.");
+      }
+      final String parent = zipFile.getParent();
+      println("Unzipping data file: " + srcZipFile + "...");
+      UnzipFiles.unzip(srcZipFile, parent);
+      if (!zipFile.exists()) {
+        throw new IllegalArgumentException("Unsuccessful Unzip.");
+      }
+      println(srcZipFile + " unzipped!");
+      dataWasZipped = true;
+    }
+  }
+
+  @Override
+  public void shutdown() {}
+
+  @Override
+  public void cleanup() {}
+
+  @Override
+  public void println(final String s) {
+    job.println(s);
+  }
+
+  // Callback
+  class Process implements ProcessLine {
+    int n = 0;
+
+    @Override
+    public void process(final String strArr0, final int lineNo) {
+      if ((lineNo % reportInterval) == 0) {
+        println("" + lineNo);
+      }
+      dataArr[n++] = Integer.parseInt(strArr0);
+    }
+  }
+
+}
--- src/main/java/com/yahoo/sketches/characterization/quantiles/MSketchStreamAProfile.java
+++ src/main/java/com/yahoo/sketches/characterization/quantiles/MSketchStreamAProfile.java
@@ -5,10 +5,6 @@
 
 package com.yahoo.sketches.characterization.quantiles;
 
-import static java.lang.Math.ceil;
-import static java.lang.Math.floor;
-import static java.lang.Math.log10;
-import static java.lang.Math.pow;
 import static java.lang.Math.rint;
 
 import java.io.File;
@@ -19,13 +15,12 @@ import com.yahoo.sketches.UnzipFiles;
 import com.yahoo.sketches.characterization.Job;
 import com.yahoo.sketches.characterization.JobProfile;
 import com.yahoo.sketches.characterization.Properties;
-import com.yahoo.sketches.characterization.quantiles.druidhistogram.ApproximateHistogram;
-import com.yahoo.sketches.characterization.quantiles.druidhistogram.Histogram;
+import com.yahoo.sketches.characterization.quantiles.momentsketch.SimpleMomentSketch;
 
 /**
  * @author Lee Rhodes
  */
-public class DruidAppHistStreamAProfile implements JobProfile {
+public class MSketchStreamAProfile implements JobProfile {
 
   private Job job;
 
@@ -33,15 +28,16 @@ public class DruidAppHistStreamAProfile implements JobProfile {
   private String srcFileName;
   private int reportInterval; //prints number of lines read to console every reportInterval lines.
   private int numRanks; //number of linearly spaced ranks between zero and one.
-  private int ppOoM; //number of split-points per Order-Of-Magnitude (OOM).
-  private int histSize; //ApproximateHistogram size. Max # of position, bin pairs
+  //private int ppOoM; //number of split-points per Order-Of-Magnitude (OOM).
+  private int moments; //number of moments
   private String cdfHdr;
   private String cdfFmt;
-  private String pmfHdr;
-  private String pmfFmt;
+  //private String pmfHdr;
+  //private String pmfFmt;
 
-  ApproximateHistogram ahist;
-  Histogram hist;
+  SimpleMomentSketch sketch;
+
+  private long n = 0;
   private boolean dataWasZipped = false;
 
   //JobProfile
@@ -53,15 +49,14 @@ public class DruidAppHistStreamAProfile implements JobProfile {
     srcFileName = prop.mustGet("FileName");
     reportInterval = Integer.parseInt(prop.mustGet("ReportInterval"));
     numRanks = Integer.parseInt(prop.mustGet("NumRanks"));
-    ppOoM = Integer.parseInt(prop.mustGet("PpOoM"));
-
+    moments = Integer.parseInt(prop.mustGet("Moments"));
     cdfHdr = prop.mustGet("CdfHdr").replace("\\t", "\t");
     cdfFmt = prop.mustGet("CdfFmt").replace("\\t", "\t");
-    pmfHdr = prop.mustGet("PdfHdr").replace("\\t", "\t");
-    pmfFmt = prop.mustGet("PdfFmt").replace("\\t", "\t");
+    //ppOoM = Integer.parseInt(prop.mustGet("PpOoM"));
+    //pmfHdr = prop.mustGet("PdfHdr").replace("\\t", "\t");
+    //pmfFmt = prop.mustGet("PdfFmt").replace("\\t", "\t");
 
-    histSize = Integer.parseInt(prop.mustGet("HistSize"));
-    ahist = new ApproximateHistogram(histSize);
+    sketch = new SimpleMomentSketch(moments);
 
     processStream();
 
@@ -91,7 +86,8 @@ public class DruidAppHistStreamAProfile implements JobProfile {
         println("" + lineNo);
       }
       final long v = Long.parseLong(strArr0);
-      ahist.offer(v);
+      sketch.add(v);
+      n++;
     }
   }
 
@@ -102,25 +98,28 @@ public class DruidAppHistStreamAProfile implements JobProfile {
     checkIfZipped(srcFileName);
 
     //Read
-    //println("");
+    println("");
     println("Input Lines Processed: ");
     final LineReader lineReader = new LineReader(srcFileName);
 
     final long startReadTime_nS = System.nanoTime();
     lineReader.read(0, new Process());
     final long readTime_nS = System.nanoTime() - startReadTime_nS;
+    println("" + n);
+    println("");
 
-    //print hist stats
-    println(ahist.toString());
+    //print sketch stats
+    println("Sketch.toString()");
+    println(sketch.toString());
+    println("");
 
     //CDF
-    final float[] fracRanks = buildRanksArr(numRanks);
+    final double[] fracRanks = buildRanksArr(numRanks);
     final long startCdfTime_nS = System.nanoTime();
-    final float[] quantiles = ahist.getQuantiles(fracRanks);
+    final double[] quantiles = sketch.getQuantiles(fracRanks);
     final long cdfTime_nS = System.nanoTime() - startCdfTime_nS;
 
     //print the cumulative rank to quantiles distribution
-    println("");
     println("CDF");
     println(String.format(cdfHdr, "Index", "Rank", "Quantile"));
     for (int i = 0; i < numRanks; i++) {
@@ -130,76 +129,69 @@ public class DruidAppHistStreamAProfile implements JobProfile {
     println("");
 
     //print PMF histogram, using Points Per Order-Of-Magnitude (ppOoM).
-    final double minV = ahist.getMin();
-    final double maxV = ahist.getMax();
-    final double n = ahist.count();
-    final float[] splitpoints = buildExpBreaksArr(minV, maxV, ppOoM);
-    //PMF
-    final long startPmfTime_nS = System.nanoTime();
-    final Histogram hist = ahist.toHistogram(splitpoints);
-
-    final double[] breaksArr = hist.getBreaks();
-    final double[] countsArr = hist.getCounts();
-    final long pmfTime_nS = System.nanoTime() - startPmfTime_nS;
-    final int lenBreaks = breaksArr.length;
-
-    println("PMF");
-    println(String.format(pmfHdr, "Index", "Quantile", "Mass"));
-    int i;
-    for (i = 0; i < (lenBreaks - 1); i++) {
-      println(String.format(pmfFmt, i, breaksArr[i], countsArr[i]));
-    }
-    println(String.format(pmfFmt, i, breaksArr[i], 0.0)); // the last point
+    //    final double minV = sketch.getMin();
+    //    final double maxV = sketch.getMax();
+    //    final double n = sketch.getN();
+    //    final double[] splitpoints = buildSplitPointsArr(max(1.0, minV), maxV, ppOoM);
+    //    //PMF
+    //    final long startPmfTime_nS = System.nanoTime();
+    //    final double[] pmfArr = sketch.getPMF(splitpoints);
+    //    final long pmfTime_nS = System.nanoTime() - startPmfTime_nS;
+    //    final int lenPMF = pmfArr.length;
+    //
+    //    println("PMF");
+    //    println(String.format(pmfHdr, "Index", "Quantile", "Mass"));
+    //    int i;
+    //    for (i = 0; i < (lenPMF - 1); i++) {
+    //      println(String.format(pmfFmt, i, splitpoints[i], pmfArr[i] * n));
+    //    }
+    //println(String.format(pmfFmt, i, maxV, pmfArr[i] * n)); // the last point
 
     final double readTime_S = readTime_nS / 1E9;
-    println("");
+
     println(String.format("ReadTime_Sec  :\t%10.3f", readTime_S));
     println(String.format("ReadRate/Sec  :\t%,10.0f", n / readTime_S));
     println(String.format("CdfTime_mSec  :\t%10.3f", cdfTime_nS / 1E6));
     println(String.format("Cdf/Point_nSec:\t%10.3f", (double)cdfTime_nS / numRanks));
-    println(String.format("PmfTime_mSec  :\t%10.3f", pmfTime_nS / 1E6));
-    println(String.format("Pmf/Point_nSec:\t%10.3f", (double)pmfTime_nS / lenBreaks));
+    //println(String.format("PmfTime_mSec  :\t%10.3f", pmfTime_nS / 1E6));
+    //println(String.format("Pmf/Point_nSec:\t%10.3f", (double)pmfTime_nS / lenPMF));
   }
 
-  /**
-   * Compute the split-points for the PMF function.
-   * This assumes all points are positive and may include zero.
-   * @param min The minimum value recorded by the sketch
-   * @param max The maximum value recorded by the sketch
-   * @param ppmag desired number of points per Order-Of-Magnitude (OOM).
-   * @return the split-points array, which may start with zero.
-   */
-  private static float[] buildExpBreaksArr(final double min, final double max, final int ppmag) {
-    final boolean minLT1 = min < 1.0;
-    final double startLgMin = minLT1 ? 1.0 : min;
-    final double log10Min = floor(log10(startLgMin));
-    final double log10Max = ceil(log10(max));
-    final int oom = (int) (log10Max - log10Min);
-    final int points = (oom * ppmag) + 1 + (minLT1 ? 1 : 0);
-    final float[] ptsArr = new float[points];
-    double sp = log10Min;
-    final double delta = 1.0 / ppmag;
-    for (int i = 0; i < points; i++) {
-      if (minLT1 && (i == 0)) { ptsArr[i] = 0; continue; }
-      ptsArr[i] = (float) pow(10, sp);
-      sp += delta;
-      sp = rint(sp * ppmag) / ppmag;
-    }
-    return ptsArr;
-  }
+  //  /**
+  //   * Compute the split-points for the PMF function.
+  //   * @param min The minimum value recorded by the sketch
+  //   * @param max The maximum value recorded by the sketch
+  //   * @param ppmag desired number of points per Order-Of-Magnitude (OOM).
+  //   * @return the split-points array
+  //   */
+  //  private static double[] buildSplitPointsArr(final double min, final double max, final int ppmag) {
+  //    final double log10Min = floor(log10(min));
+  //    final double log10Max = ceil(log10(max));
+  //    final int oom = (int) (log10Max - log10Min);
+  //    final int points = (oom * ppmag) + 1;
+  //    final double[] ptsArr = new double[points];
+  //    double sp = log10Min;
+  //    final double delta = 1.0 / ppmag;
+  //    for (int i = 0; i < points; i++) {
+  //      ptsArr[i] = pow(10, sp);
+  //      sp += delta;
+  //      sp = rint(sp * ppmag) / ppmag;
+  //    }
+  //    return ptsArr;
+  //  }
 
   /**
    * Compute the ranks array.
-   * @param numRanks the number of evenly-spaced rank values excluding 0 and 1.0.
+   * @param numRanks the number of evenly-spaced rank values including 0 and 1.0.
    * @return the ranks array
    */
-  private static float[] buildRanksArr(final int numRanks) {
-    final int numRM1 = numRanks + 1;
-    final float[] fractions = new float[numRanks];
+  private static double[] buildRanksArr(final int numRanks) {
+    final int numRM1 = numRanks - 1;
+    final double[] fractions = new double[numRanks];
     final double delta = 1.0 / (numRM1);
-    double d = delta;
+    double d = 0.0;
     for (int i = 0; i < numRanks; i++) {
-      fractions[i] = (float) d;
+      fractions[i] = d;
       d += delta;
       d = rint(d * numRM1) / numRM1;
     }
