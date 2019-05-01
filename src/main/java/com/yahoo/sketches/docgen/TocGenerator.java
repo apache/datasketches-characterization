68c19ab237587b93011dd74d801fe0486d36eb4f
Lee Rhodes <leerho@users.noreply.github.com>
2019-05-01 15:57:12 -0700

    Update DocGen

--- src/main/java/com/yahoo/sketches/docgen/TocGenerator.java
+++ src/main/java/com/yahoo/sketches/docgen/TocGenerator.java
@@ -5,11 +5,14 @@
 
 package com.yahoo.sketches.docgen;
 
+import static com.yahoo.sketches.Files.openPrintWriter;
+
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.testng.annotations.Test;
 
 import com.yahoo.sketches.Files;
 
@@ -17,24 +20,53 @@ import com.yahoo.sketches.Files;
  * @author Lee Rhodes
  */
 public class TocGenerator {
-  static final String LS = System.getProperty("line.separator");
-  int level = 0;
+  private static final String LS = System.getProperty("line.separator");
+  private int level = 0;
+  private PrintWriter pw = null;
+  private String jsonScrFile;
+  private String htmlScriptFile;
 
-  public TocGenerator() {}
+  /**
+   *
+   * @param jsonSrcFile The JSON source file
+   * @param htmlScriptFile The javascript source file
+   * @param tgtTocFile the target toc.html file
+   */
+  public TocGenerator(final String jsonSrcFile, final String htmlScriptFile, final String tgtTocFile) {
+    jsonScrFile = jsonSrcFile;
+    this.htmlScriptFile = htmlScriptFile;
+    if ((tgtTocFile != null) && (!tgtTocFile.isEmpty())) {
+      final File file = new File(tgtTocFile);
+      if (file.exists()) { file.delete(); }
+      pw = openPrintWriter(tgtTocFile);
+    }
+  }
 
-  @Test
+  /**
+   * Reads the JSON source file and the html script file and generates the target toc.html file.
+   */
   public void readJson() {
     final StringBuilder sb = new StringBuilder();
-    final String jin = Files.fileToString("src/main/resources/docgen/toc.json");
+    final String jin = Files.fileToString(jsonScrFile);
     final JSONObject jo = new JSONObject(jin);
     final String clazz = jo.getString("class");
     if (clazz.equals("TOC")) { emitToc(jo, sb); }
     else if (clazz.equals("Dropdown")) { emitDropdown(jo, sb); }
     else { emitDoc(jo, sb); }
+    if ((htmlScriptFile != null) && (!htmlScriptFile.isEmpty())) {
+      final String script = Files.fileToString(htmlScriptFile);
+      sb.append(script);
+    }
     println(sb.toString());
   }
 
+  /**
+   * Generates an entire toc.html in a StringBuilder
+   * @param toc the input JSON object
+   * @param sb the target StringBuilder
+   */
   void emitToc(final JSONObject toc, final StringBuilder sb) {
+    sb.append("<!-- Computer Generated File, Do Not Edit! -->").append(LS);
     sb.append("<link rel=\"stylesheet\" href=\"/css/toc.css\">").append(LS);
     sb.append("<div id=\"toc\" class=\"nav toc hidden-print\">").append(LS);
 
@@ -53,6 +85,12 @@ public class TocGenerator {
     sb.append("</div>").append(LS);
   }
 
+  /**
+   * Generates a Dropdown object from the input JSON source.
+   * This is a recursive co-routine with the emitDoc() function.
+   * @param dropdn the input JSON
+   * @param sb the target StringBuilder
+   */
   void emitDropdown(final JSONObject dropdn, final StringBuilder sb) {
     final String desc = dropdn.getString("desc");
     final String lowercaseDesc = desc.toLowerCase();
@@ -62,7 +100,7 @@ public class TocGenerator {
     final String indent = indent(level);
     //paragraph with desc
     sb.append(LS);
-    sb.append(indent).append("<p id=").append(quotes(pId)).append(LS);
+    sb.append(indent).append("<p id=").append(quotes(pId)).append(">").append(LS);
     sb.append(indent).append("  ").append("<a data-toggle=\"collapse\" ")
       .append("class=\"menu collapsed\" href=").append(quotes(href)).append(">")
       .append(desc).append("</a>").append(LS);
@@ -86,7 +124,12 @@ public class TocGenerator {
     sb.append(indent).append("</div>").append(LS);
   }
 
-
+  /**
+   * Generates a Document object from the input JSON source.
+   * This is a recursive co-routine with the emitDropdown() function.
+   * @param doc the input JSON
+   * @param sb the target StringBuilder
+   */
   void emitDoc(final JSONObject doc, final StringBuilder sb) {
     final String dir = doc.getString("dir");
     final String file = doc.getString("file");
@@ -109,7 +152,11 @@ public class TocGenerator {
     sb.append("</a></li>").append(LS);
   }
 
-
+  /**
+   * Encases the given string in quotes.
+   * @param s the given string
+   * @return the quoted string
+   */
   public static String quotes(final String s) {
     return '"' + s + '"';
   }
@@ -127,6 +174,54 @@ public class TocGenerator {
     return sb.toString();
   }
 
-  static void println(final String s) { System.out.println(s); }
+
+  /**
+   * The JVM may call this method to close the PrintWriter resource.
+   */
+  @Override
+  protected void finalize() throws Throwable {
+    try {
+      if (pw != null) {
+        pw.close(); // close open files
+      }
+    } finally {
+      super.finalize();
+    }
+  }
+
+  /**
+   * Outputs a line to the configured PrintWriter and stdOut.
+   * @param s The String to print
+   */
+  public final void println(final String s) {
+    System.out.println(s);
+    if (pw != null) {
+      pw.println(s);
+      pw.flush();
+    }
+  }
+
+  /**
+   * Flush any buffered output to the configured PrintWriter.
+   */
+  public final void flush() {
+    if (pw != null) { pw.flush(); }
+  }
+
+  /**
+   * Command line access.
+   * @param args three arguments are required:
+   * <ol><li>The JSON source file</li>
+   * <li>The html script file that is appended to the end.</li>
+   * <li>The target toc.html file</li>
+   * </ol>
+   */
+  public static void main(final String[] args) {
+    final String jsonSrcFile = args[0];
+    final String htmlScriptFile = args[1];
+    final String tgtTocFile = args[2];
+    final TocGenerator tocgen = new TocGenerator(jsonSrcFile, htmlScriptFile, tgtTocFile);
+    tocgen.readJson();
+  }
 
 }
